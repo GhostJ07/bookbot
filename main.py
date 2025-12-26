96875a907aa8a0df9c14b9ddcdda88ab75de28a1
Gilles Decock <Gilles.decock@hotmail.com>
2025-12-26 22:20:54 +0100

    a description of your changes here

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_counter, char_counter, sorted_letters
+import sys
 import string
 
 def get_book_text(path):
@@ -6,20 +7,24 @@ def get_book_text(path):
             return f.read()
 
 def main ():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    wordCount = word_counter(text)
-    charCount = char_counter(text)
+    if  len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:     
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        wordCount = word_counter(text)
+        charCount = char_counter(text)
 
-    print(f"============ BOOKBOT ============\n")
-    
-    print(f"Analyzing book found at {book_path}...\n")
-    print("----------- Word Count ----------\n")
-    print(f"Found {wordCount} total words")
-    print("--------- Character Count -------\n")
-    for letter in sorted_letters(charCount):
-        print(f"{letter['letter']}: {letter['num']}")
-    
-    print("\n============= END ===============\n")
+        print(f"============ BOOKBOT ============\n")
+        
+        print(f"Analyzing book found at {book_path}...\n")
+        print("----------- Word Count ----------\n")
+        print(f"Found {wordCount} total words")
+        print("--------- Character Count -------\n")
+        for letter in sorted_letters(charCount):
+            print(f"{letter['letter']}: {letter['num']}")
+        
+        print("\n============= END ===============\n")
     
 main()
